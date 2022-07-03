564afc7a46a212372383cfedbb36bef8d8b33dd2
futurework <75208664+JoeyRandsdorp@users.noreply.github.com>
2022-07-03 20:53:39 +0200

    Add files via upload

---
+++ magazinepage.php
@@ -0,0 +1,37 @@
+<?php
+    include_once "webservices/includes/actions.php";
+?>
+
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Pokemon Magazine</title>
+        <link rel="stylesheet" href="css/makeUp.css">
+        <script type="text/javascript" src="js/main.js" defer></script>
+    </head>
+    <body>
+        <div class="menubar">                                   <!-- menubar -->
+                <img src="images/logo.jpg" class="logo">
+                <img src="images/title.png" class="titlePic">
+                <img src="images/door.png" class="door">
+        </div>
+        <div class="magazine">                                              
+            <aside class="teamBox">                           <!-- left box -->
+                <section class="monTeam">
+                    <p class="teamTitle">Your team:</p>
+                
+                    <div class="bronnen">
+                        <li><a href="sources.html">Bronnen van plaatjes</a></li>
+                    </div>
+                </section>    
+            </aside>
+            <main id="pokedexBox">                              <!-- middlebox  -->
+
+            </main>
+            <aside class="monBox">                                <!-- rightbox -->
+            
+                </section>    
+            </aside>
+        </div>
+    </body>
+</html>
