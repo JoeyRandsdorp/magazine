de481e885554d083cae0cb49fc7ba0f9fe743194
futurework <75208664+JoeyRandsdorp@users.noreply.github.com>
2022-07-03 20:53:55 +0200

    Add files via upload

--- index.php
+++ index.php
@@ -1,37 +1,13 @@
 <?php
-    include_once "webservices/includes/actions.php";
-?>
-
-<!DOCTYPE html>
-<html>
-    <head>
-        <title>Pokemon Magazine</title>
-        <link rel="stylesheet" href="css/makeUp.css">
-        <script type="text/javascript" src="js/main.js" defer></script>
-    </head>
-    <body>
-        <div class="menubar">                                   <!-- menubar -->
-                <img src="images/logo.jpg" class="logo">
-                <img src="images/title.png" class="titlePic">
-                <img src="images/door.png" class="door">
-        </div>
-        <div class="magazine">                                              
-            <aside class="teamBox">                           <!-- left box -->
-                <section class="monTeam">
-                    <p class="teamTitle">Your team:</p>
-                
-                    <div class="bronnen">
-                        <li><a href="sources.html">Bronnen van plaatjes</a></li>
-                    </div>
-                </section>    
-            </aside>
-            <main id="pokedexBox">                              <!-- middlebox  -->
-
-            </main>
-            <aside class="monBox">                                <!-- rightbox -->
-            
-                </section>    
-            </aside>
-        </div>
-    </body>
-</html>
+//Require functions for actions
+require_once "includes/actions.php";
+//Based on the existence of the GET parameter, 1 of the 2 functions will be called
+if (!isset($_GET['id'])) {
+    $data = getPokemon();
+} else {
+    $data = getPokemonDetails($_GET['id']);
+}
+//Set the header & output JSON so the client will know what to expect.
+header("Content-Type: application/json");
+echo json_encode($data);
+exit;
